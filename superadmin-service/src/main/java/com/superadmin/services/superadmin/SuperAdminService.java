7e553cedfc3973a6f1a7fe0a6a939e82158f29ec
AzerArfa <120283616+AzerArfa@users.noreply.github.com>
2024-04-29 19:36:52 +0200

    Add files via upload

---
+++ superadmin-service/src/main/java/com/superadmin/services/superadmin/SuperAdminService.java
@@ -0,0 +1,11 @@
+package com.superadmin.services.superadmin;
+
+
+import java.util.UUID;
+
+import com.superadmin.dto.UserDto;
+
+public interface SuperAdminService {
+
+    UserDto makeAdmin(UUID id);
+}
---
+++ superadmin-service/src/main/java/com/superadmin/services/superadmin/SuperAdminServiceImpl.java
@@ -0,0 +1,21 @@
+package com.superadmin.services.superadmin;
+
+import com.superadmin.dto.UserDto;
+import com.superadmin.services.feign.AuthService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SuperAdminServiceImpl implements SuperAdminService {
+
+    private final AuthService authService;
+
+    @Override
+    public UserDto makeAdmin(UUID id) {
+        return authService.makeAdmin(id).getBody();
+    }
+}
