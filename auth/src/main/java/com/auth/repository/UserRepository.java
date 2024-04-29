42e03bb5da7353891c02804c5703fbdd1aaad3b0
AzerArfa <120283616+AzerArfa@users.noreply.github.com>
2024-04-29 19:35:31 +0200

    Add files via upload

---
+++ auth/src/main/java/com/auth/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.auth.repository;
+
+import com.auth.entity.User;
+import com.auth.enums.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findFirstByEmail(String email);
+
+    User findByRole(UserRole userRole);
+}
